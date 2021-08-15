88acf5d3fba781382e006bdc412a7cb9d022cfc9
Kim Yeseul <imys0613@naver.com>
2021-08-15 16:40:46 +0900

    [UPDATE] 초기 map 설정 undefined 문제  해결

--- src/views/MapView/Map.tsx
+++ src/views/MapView/Map.tsx
@@ -7,18 +7,27 @@ import * as _ from "lodash";
 const REFETCH_INTERVAL_MS = 1000;
 const RESIZE_DELAY_MS = 300;
 
+// TODO: 스타일 적용
+const contentTemplate = (position: Position) =>
+  `고도<br>` +
+  `${position.lat}<br>` +
+  `<br>` +
+  `업데이트 시각<br>` +
+  `${position.dt.toLocaleDateString()}<br>` +
+  `${position.dt.toLocaleTimeString()}`;
+
 type Props = {
   targetId: number;
 };
 
 // Map container
 const Map: React.FC<Props> = ({ targetId }) => {
-  const [map, setMap] = useState<any>(null);
-  const [position, setPosition] = useState<Position>(new Position());
+  const [map, setMap] = useState<any>();
+  const [position, setPosition] = useState<Position | null>();
 
-  // TODO: 데이터 패치 로직 분리하기
+  // TODO: 데이터 패치 및 적용 로직 분리하기
   // Fetch user position data
-  const { isLoading, data, error, isFetching } = useQuery(
+  const { data, error, isFetching } = useQuery(
     "position",
     () => getPosition(targetId),
     {
@@ -27,73 +36,65 @@ const Map: React.FC<Props> = ({ targetId }) => {
     }
   );
 
-  // Set position
-  // FIXME: 정상적인 데이터가 입력되면 확인
+  // Set position every fetch
   useEffect(() => {
     if (!isFetching) {
+      // TODO: 정상 데이터가 들어올 때 주석 해제
       let _position = new Position();
       // data.latitude,
       // data.longitude,
       // data.altitude,
-      // new Date(data.recent_upload)
+      // new Date()
 
       setPosition(_position);
     }
   }, [isFetching]);
 
-  // Initialize map
+  // Initialize map when fetched and empty map
   useEffect(() => {
-    if (isLoading === false) {
-      let center = new window.kakao.maps.LatLng(position.lat, position.lng);
+    if (!map && position) {
+      let _center = new window.kakao.maps.LatLng(position.lat, position.lng);
 
-      const container = document.getElementById("map");
-      const options = {
-        center: center,
+      const _container = document.getElementById("map");
+      const _options = {
+        center: _center,
         level: 3,
       };
 
-      setMap(new window.kakao.maps.Map(container, options));
+      let _map = new window.kakao.maps.Map(_container, _options);
 
       // Relayout map when window resized using debounce
-      const onResize = () => map.relayout();
+      const _onResize = () => _map.relayout();
 
       document.addEventListener(
         "resize",
-        _.debounce(onResize, RESIZE_DELAY_MS)
+        _.debounce(_onResize, RESIZE_DELAY_MS)
       );
-    }
-  }, [isLoading]);
 
-  // TODO: 스타일 적용
+      setMap(_map);
+    }
+  }, [position]);
+
   // Draw marker and infoWindow
   useEffect(() => {
-    if (isLoading === false) {
-      let markerPosition = new window.kakao.maps.LatLng(
+    if (map && position) {
+      let _markerPosition = new window.kakao.maps.LatLng(
         position.lat,
         position.lng
       );
 
-      let marker = new window.kakao.maps.Marker({
-        position: markerPosition,
+      let _marker = new window.kakao.maps.Marker({ position: _markerPosition });
+      let _infoWindow = new window.kakao.maps.InfoWindow({
+        position: _markerPosition,
+        content: contentTemplate(position),
       });
 
-      let infoWindow = new window.kakao.maps.InfoWindow({
-        position: markerPosition,
-        content:
-          `고도<br>` +
-          `${position.alt}<br>` +
-          `<br>` +
-          `업데이트 시각<br>` +
-          `${position.dt.toLocaleDateString()}<br>` +
-          `${position.dt.toLocaleTimeString()}`,
-      });
-
-      marker.setMap(map);
-      infoWindow.open(map, marker);
+      _marker.setMap(map);
+      _infoWindow.open(map, _marker);
 
       return () => {
-        marker.setMap(null);
-        infoWindow.setMap(null);
+        _marker.setMap(null);
+        _infoWindow.setMap(null);
       };
     }
   }, [position]);
