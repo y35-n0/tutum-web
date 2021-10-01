07d9e1513d933e17e537877481da37eed7aa1bce
Kim Yeseul <imys0613@naver.com>
2021-10-02 02:14:34 +0900

    [UPDATE] Fetch data with react-query with dummy data

--- src/api/workerApi.ts
+++ src/api/workerApi.ts
@@ -1,6 +1,14 @@
-import axios from "axios";
+// import axios from "axios";
+import { user_position_json } from "../test/jsonData";
 
-export const getPosition = async (id: number) => {
-  const res = await axios.get(`/api/worker/get/loc/${id}`);
-  return res.data;
+// export const getPosition = async (id: number) => {
+//   const res = await axios.get(`/api/worker/get/loc/${id}`);
+//   return res.data;
+// };
+
+export const getMap = async (id: number) => {
+  let res = null;
+  await setTimeout(() => {}, 1000);
+  res = user_position_json;
+  return res;
 };
--- src/views/MapView/Map.tsx
+++ src/views/MapView/Map.tsx
@@ -1,13 +1,13 @@
 import React, { useState, useEffect, useRef, useCallback } from "react";
 import { select } from "d3";
 import { scaleLinear } from "d3-scale";
+import { useQuery } from "react-query";
 import { deserialize } from "typescript-json-serializer";
-import { Building, Floor, Position } from "../../@types/MapAPITypes";
-import {
-  building_info_json,
-  floor_info_json,
-  user_position_json,
-} from "../../test/json_data";
+import { Position } from "../../@types/MapAPITypes";
+import { user_position_json } from "../../test/jsonData";
+import { getMap } from "../../api/workerApi";
+
+const REFETCH_INTERVAL_MS = 1000;
 
 type MapProps = {
   targetId: number;
@@ -15,56 +15,58 @@ type MapProps = {
 
 const Map: React.FC<MapProps> = ({ targetId }) => {
   // TODO: 실제 값 가져오기
-  const [building, setBuilding] = useState<Building>(() => {
-    return deserialize<Building>(building_info_json, Building);
-  });
-  const [floor, setFloor] = useState<Floor>(() => {
-    return deserialize<Floor>(floor_info_json, Floor);
-  });
+  // Fetch position data
+  const { data /* , error, isFetching */ } = useQuery(
+    "position",
+    () => getMap(targetId),
+    {
+      refetchInterval: REFETCH_INTERVAL_MS,
+      onSuccess: (data) => {
+        setPosition(deserialize<Position>(data, Position));
+      },
+    }
+  );
+
+  // TODO: position 최초 설정 수정 (default X)
   const [position, setPosition] = useState<Position>(() => {
     return deserialize<Position>(user_position_json, Position);
   });
   const svgRef = useRef(null);
-
-  // TODO: 최초 설정
-  // - interval로 값 가져오기
-  // - Deserialize buliding, floor, position data
-  useEffect(() => {}, []);
 
   // Scale
   // When floor data updated, update scale functions.
   const xScale = useCallback(
     (x) => {
       const xScale = scaleLinear()
-        .domain([0, building.size.width])
+        .domain([0, position.building.size.width])
         .range([
-          floor.img.padding.left,
-          floor.img.size.width - floor.img.padding.right,
+          position.floor.img.padding.left,
+          position.floor.img.size.width - position.floor.img.padding.right,
         ]);
       return xScale(x);
     },
-    [floor]
+    [position.floor, position.building]
   );
 
   const yScale = useCallback(
     (y) => {
       const yScale = scaleLinear()
-        .domain([building.size.height, 0])
+        .domain([position.building.size.height, 0])
         .range([
-          floor.img.size.height - floor.img.padding.top,
-          floor.img.padding.bottom,
+          position.floor.img.size.height - position.floor.img.padding.top,
+          position.floor.img.padding.bottom,
         ]);
       return yScale(y);
     },
-    [floor]
+    [position.floor, position.building]
   );
 
   // Map Image
   // When floor data updated, draw floor map image
   useEffect(() => {
     const svg = select(svgRef.current)
-      .attr("width", floor.img.size.width)
-      .attr("height", floor.img.size.height);
+      .attr("width", position.floor.img.size.width)
+      .attr("height", position.floor.img.size.height);
 
     // image
     svg
@@ -72,10 +74,10 @@ const Map: React.FC<MapProps> = ({ targetId }) => {
       .attr("id", "background_map_img")
       .attr("x", 0)
       .attr("y", 0)
-      .attr("width", floor.img.size.width)
-      .attr("height", floor.img.size.height)
-      .attr("xlink:href", floor.img.url);
-  }, [floor]);
+      .attr("width", position.floor.img.size.width)
+      .attr("height", position.floor.img.size.height)
+      .attr("xlink:href", position.floor.img.url);
+  }, [position.floor]);
 
   // Position Marker
   // When position data or floor data updated, draw position marker.
@@ -86,13 +88,13 @@ const Map: React.FC<MapProps> = ({ targetId }) => {
       .append("circle")
       .attr("id", "user_position")
       .attr("cx", xScale(position.x))
-      .attr("cy", floor.img.size.height - yScale(position.y))
+      .attr("cy", position.floor.img.size.height - yScale(position.y))
       .attr("r", 8)
       .style("fill", "red")
       .style("stroke", "black");
 
     // TODO: 시간 표시
-  }, [position, floor]);
+  }, [position, xScale, yScale]);
 
   return <svg ref={svgRef}></svg>;
 };
