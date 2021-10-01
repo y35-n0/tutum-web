2d8f3f723adab7286bb00a4a9bbe8578467008d3
Kim Yeseul <imys0613@naver.com>
2021-10-02 00:04:35 +0900

    [UPDATE] Draw map and position with dummy data

--- src/views/MapView/Map.tsx
+++ src/views/MapView/Map.tsx
@@ -1,40 +1,66 @@
-import React, { useState, useEffect, useRef } from "react";
-import { Building, Floor, Position } from "../../@types/MapAPITypes";
+import React, { useState, useEffect, useRef, useCallback } from "react";
 import { select } from "d3";
-import { ScaleLinear, scaleLinear } from "d3-scale";
+import { scaleLinear } from "d3-scale";
+import { deserialize } from "typescript-json-serializer";
+import { Building, Floor, Position } from "../../@types/MapAPITypes";
 import {
   building_info_json,
   floor_info_json,
   user_position_json,
 } from "../../test/json_data";
-import { deserialize } from "typescript-json-serializer";
 
 type MapProps = {
   targetId: number;
 };
 
 const Map: React.FC<MapProps> = ({ targetId }) => {
-  // FIXME: 실제 값 가져오기
-  const [position, setPosition] = useState<Position>(() => {
-    return deserialize<Position>(user_position_json, Position);
+  // TODO: 실제 값 가져오기
+  const [building, setBuilding] = useState<Building>(() => {
+    return deserialize<Building>(building_info_json, Building);
   });
   const [floor, setFloor] = useState<Floor>(() => {
     return deserialize<Floor>(floor_info_json, Floor);
   });
-
-  const [building, setBuilding] = useState<Building>(() => {
-    return deserialize<Building>(building_info_json, Building);
+  const [position, setPosition] = useState<Position>(() => {
+    return deserialize<Position>(user_position_json, Position);
   });
   const svgRef = useRef(null);
-  // FIXME: current read-only error
-  const x = useRef<ScaleLinear<number, number, never>>(null);
-  const y = useRef<ScaleLinear<number, number, never>>(null);
 
   // TODO: 최초 설정
   // - interval로 값 가져오기
+  // - Deserialize buliding, floor, position data
   useEffect(() => {}, []);
 
-  // TODO: floor 변경되었을 때 업데이트
+  // Scale
+  // When floor data updated, update scale functions.
+  const xScale = useCallback(
+    (x) => {
+      const xScale = scaleLinear()
+        .domain([0, building.size.width])
+        .range([
+          floor.img.padding.left,
+          floor.img.size.width - floor.img.padding.right,
+        ]);
+      return xScale(x);
+    },
+    [floor]
+  );
+
+  const yScale = useCallback(
+    (y) => {
+      const yScale = scaleLinear()
+        .domain([building.size.height, 0])
+        .range([
+          floor.img.size.height - floor.img.padding.top,
+          floor.img.padding.bottom,
+        ]);
+      return yScale(y);
+    },
+    [floor]
+  );
+
+  // Map Image
+  // When floor data updated, draw floor map image
   useEffect(() => {
     const svg = select(svgRef.current)
       .attr("width", floor.img.size.width)
@@ -42,7 +68,6 @@ const Map: React.FC<MapProps> = ({ targetId }) => {
 
     // image
     svg
-      .select("#background_map_img")
       .append("image")
       .attr("id", "background_map_img")
       .attr("x", 0)
@@ -50,35 +75,25 @@ const Map: React.FC<MapProps> = ({ targetId }) => {
       .attr("width", floor.img.size.width)
       .attr("height", floor.img.size.height)
       .attr("xlink:href", floor.img.url);
-
-    // scale
-    x.current = scaleLinear()
-      .domain([0, building.size.width])
-      .range([
-        floor.img.padding.left,
-        floor.img.size.width - floor.img.padding.right,
-      ]);
-
-    y.current = scaleLinear()
-      .domain([building.size.height, 0])
-      .range([
-        floor.img.size.height - floor.img.padding.top,
-        floor.img.padding.bottom,
-      ]);
   }, [floor]);
 
-  // TODO: position 변경되었을 때 업데이트
+  // Position Marker
+  // When position data or floor data updated, draw position marker.
   useEffect(() => {
     const svg = select(svgRef.current);
-
     // position
     svg
-      .select("#user_position")
       .append("circle")
       .attr("id", "user_position")
-      .attr("cx", x(position.x))
-      .attr("cy", floor.img.size.height - position.y);
+      .attr("cx", xScale(position.x))
+      .attr("cy", floor.img.size.height - yScale(position.y))
+      .attr("r", 8)
+      .style("fill", "red")
+      .style("stroke", "black");
+
+    // TODO: 시간 표시
   }, [position, floor]);
+
   return <svg ref={svgRef}></svg>;
 };
 
