ac48d4717decd0068eea36f2d758ef56caf2883b
Kim Yeseul <imys0613@naver.com>
2021-10-22 22:49:23 +0900

    [UPDATE] show two popups

--- src/views/PopupView/PopupSide.tsx
+++ src/views/PopupView/PopupSide.tsx
@@ -3,16 +3,13 @@ import { css } from "@emotion/react";
 
 const style = css`
   z-index: 10;
+  position: absolute;
   right: 0;
   top: 0;
-  margin: 10px;
-  padding: 10px;
-  border-radius: 20px;
-  background-color: #fff;
-  box-shadow: var(--box-shadow);
 `;
 
-const PopupBox: React.FC = (props) => {
+const PopupSide: React.FC = (props) => {
   return <div css={style}>{props.children}</div>;
 };
-export default PopupBox;
+
+export default PopupSide;
