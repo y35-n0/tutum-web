7ff9d22acf3fe8b1ef35b46db5f37c4151180e48
Kim Yeseul <imys0613@naver.com>
2021-11-13 20:07:20 +0900

    [UPDATE] clean react query code

--- src/index.tsx
+++ src/index.tsx
@@ -1,22 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
-import { ReactQueryDevtools } from "react-query/devtools";
 import { RecoilRoot } from "recoil";
 
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
-const queryClient = new QueryClient();
-
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      {/* <QueryClientProvider client={queryClient}> */}
       <App />
-      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
-      {/* </QueryClientProvider> */}
     </RecoilRoot>
   </React.StrictMode>,
   document.getElementById("root")
