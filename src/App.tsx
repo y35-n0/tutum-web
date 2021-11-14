00f9c9619bed61477724040019b3dd29fe66a16e
Kim Yeseul <imys0613@naver.com>
2021-11-14 10:20:26 +0900

    [UPDATE] Apply react-query to abnormal states

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,8 @@ import { useEffect } from "react";
 import { useSetRecoilState } from "recoil";
 import "./App.css";
 import { abnormalStatesAtom } from "./atoms/abnormalStatesAtoms";
-import useAbnormalStates from "./hooks/useAbnormalStates";
+import AbnormalStatesQuery from "./queries/AbnormalStatesQuery";
+import useAbnormalStates from "./queries/AbnormalStatesQuery";
 import GlobalStyle from "./views/common/GlobalStyle";
 import Header from "./views/common/Header";
 import NavigatorAndTime from "./views/common/NavigatorAndTime";
@@ -11,29 +12,17 @@ import OutWrapper from "./views/common/Wrapper/OutWrapper";
 import DashboradView from "./views/DashboardView";
 import PopupView from "./views/PopupView";
 
-const REFETCH_INTERVAL_MS = 1000;
-
 function App() {
-  const setAbnormalStates = useSetRecoilState(abnormalStatesAtom);
-  const [updatedAbnormalStates, updateAbnormalStates] = useAbnormalStates();
-
   useEffect(() => {
     document.title = "Dashboard - TUTUM";
   }, []);
-
-  useEffect(() => {
-    setAbnormalStates(updatedAbnormalStates);
-    const timer = setInterval(async () => {
-      await updateAbnormalStates();
-    }, REFETCH_INTERVAL_MS);
-    return () => clearInterval(timer);
-  }, [updatedAbnormalStates, updateAbnormalStates, setAbnormalStates]);
 
   // get and set state atom
 
   // TODO: wrap abnormal states management
   return (
     <div className="App">
+      <AbnormalStatesQuery />
       <GlobalStyle />
       <Header />
       <OutWrapper>
--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 import { RecoilRoot } from "recoil";
 
 import App from "./App";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 
+const queryClient = new QueryClient();
+
 ReactDOM.render(
   <React.StrictMode>
-    <RecoilRoot>
-      <App />
-    </RecoilRoot>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <RecoilRoot>
+        <App />
+      </RecoilRoot>
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
